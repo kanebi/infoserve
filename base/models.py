a242067ebfece230217718e0844f9d5f8f3c661c
ninotrabajador <tobipeternwakaji2000@yahoo.com>
2021-10-08 02:45:30 -0700

    tobe 4th commit

--- base/models.py
+++ base/models.py
@@ -1,17 +1,1 @@
 from django.db import models
-
-class tobetest(nodels,class tobe(models.Model):
-
-    name = models.CharField(, max_length=50)
-
-    class Meta:
-        verbose_name = _("tobe")
-        verbose_name_plural = _("tobes")
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("tobe_detail", kwargs={"pk": self.pk})
-)
-# Create your models here. tobe added this
