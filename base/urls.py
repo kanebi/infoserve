930d368384bcc06a3e9af532baffea8efe80ebd3
ninotrabajador <tobipeternwakaji2000@yahoo.com>
2021-10-25 07:26:03 -0700

    tobe contact

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index, index
+from .views import index, contact
 urlpatterns = [
-    path("", index, name="home")
+    path("", index, name="home"),
+     path("contact-us", contact, name="contact-us")
 ]
--- base/views.py
+++ base/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    print('hello world')
+
     return render(request, template_name='index.html')
+def contact(request):
+
+    return render(request, template_name='contact.html')
 # Create your views here.
